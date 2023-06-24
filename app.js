4d4389dc7311900b9c423480cba2dc62d22d69af
mehmet <tas349873@gmail.com>
2023-06-24 13:26:00 +0300

    app.js

--- app.js
+++ app.js
@@ -3,3 +3,20 @@ const API_URL =
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API =
   'https://api.themoviedb.org/4/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+
+  const form = document.getElementById('form')
+  const search = document.getElementById('search')
+  const main = document.getElementById('main')
+  
+  getMovies(API_URL)
+
+  async function getMovies(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.results);
+  }
+
+
+  form.addEventListener("submit",(e) =>{
+    e.preventDefault();
+  })
