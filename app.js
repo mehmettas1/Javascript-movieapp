049377e4e4691db026c05aa0786aefc2f3aa459f
mehmet <tas349873@gmail.com>
2023-06-24 13:45:27 +0300

    update

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ form.addEventListener("submit", (e) => {
 });
 
 function showMovies(movies) {
+  const main = "";
   movies.forEach((movie)=>{
     const { title ,poster_path,vote_average,overview} = movie;
 
@@ -38,6 +39,34 @@ function showMovies(movies) {
     movieEl.classList.add("movie");
 
     movieEl.innerHTML = `
-    `
-  })
+    <img
+    src="${IMG_PATH + poster_path}" alt="${title}"
+  />
+  <div class="movie-info">
+    <h3>${title}</h3>
+    <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+  </div>
+  <div class="overview">
+    <h3> ${title}  <small> Overview </small> </h3>
+    <p>
+     ${overview}
+    </p>
+  </div>
+  
+  `
+
+  main.appendChild(movieEl);
+})
+
+}
+
+function  getClassByRate(vote){
+  if(vote >= 8){
+    return "green";
+  } else if(vote >=5){
+    return "orange";
+
+  } else {
+    return "red";
+  }
 }
